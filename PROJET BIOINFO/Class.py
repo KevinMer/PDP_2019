96666ca5f4ae72fc7686154f68b63a8b5a6f6b2c
Kevin Merchadou <merchadou.kevin@gmail.com>
2019-03-20 17:38:17 +0100

    Class

--- PROJET BIOINFO/Class.py
+++ PROJET BIOINFO/Class.py
@@ -1,21 +1,28 @@
 class LignePatient:
 
-    Marqueur = None
-    Allele = []
-    Hauteur = []
+    def __init__(self,marqueur,allele,hauteur):
+        self.marqueur = marqueur
+        self.allele = allele
+        self.hauteur = hauteur
 
 class Mere(LignePatient):
 
-    Homozygote = False
-    Semblable = False
+    def __init__(self,marqueur,allele,hauteur,homozygote):
+        super().__init__(marqueur,allele,hauteur)
+        self.homozygote = False
+
 
 class Foetus(LignePatient):
 
-    Echo = False
-    Semblable = False
+    def __init__(self,echo,semblable,contamination):
+        super().__init__()
+        self.echo = False
+        self.semblable = False
+        self.contamination = "pas_contamine"
 
 class Pere(LignePatient):
-    pass
+    def __init__(self):
+        super().__init__()
 
-
-print(Foetus.Echo)
+machin= Mere("truc","all1",36,True)
+print(machin.marqueur)
