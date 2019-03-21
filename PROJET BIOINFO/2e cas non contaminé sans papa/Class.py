fe847e5ac722b277e884a5eef3c2b9ea8cb5428e
Kevin Merchadou <merchadou.kevin@gmail.com>
2019-03-21 18:19:05 +0100

    Class traitement avance

--- PROJET BIOINFO/2e cas non contaminé sans papa/Class.py
+++ PROJET BIOINFO/2e cas non contaminé sans papa/Class.py
@@ -3,29 +3,57 @@ import pandas as pd
 
 class Patient:
 
-    def __init__(self,marqueur,allele,hauteur):
+    def __init__(self,marqueur,allele,hauteur,informatif):
         self.marqueur = marqueur
         self.allele = allele
         self.hauteur = hauteur
+        self.informatif = informatif
+
+    def allele_semblable(self,foetus):
+        Semblable = True
+        for Allele in range(3):
+            if str(self.allele[Allele]) != str(foetus.allele[Allele]):
+                Semblable = False
+                break
+        return Semblable
+
 
 class Mere(Patient):
 
-    def __init__(self,marqueur,allele,hauteur,homozygote):
-        super().__init__(marqueur,allele,hauteur)
+    def __init__(self,marqueur,allele,hauteur,informatif,homozygote):
+        super().__init__(marqueur,allele,hauteur,informatif)
         self.homozygote = homozygote
+
+    def homozygotie(self):
+        if "nan" in str(self.allele[1]):
+            self.homozygote = True
 
 
 class Foetus(Patient):
 
-    def __init__(self,marqueur,allele,hauteur,echo,semblable,contamination):
-        super().__init__(marqueur,allele,hauteur)
+    def __init__(self,marqueur,allele,hauteur,informatif,echo,semblable,contamination):
+        super().__init__(marqueur,allele,hauteur,informatif)
         self.echo = echo
         self.semblable = semblable
         self.contamination = contamination
+    
+    def foetus_3_pics(self):
+        if "nan" not in str(self.allele):
+            self.contamination = "contamine"
+            #contamination_heterozygote(self)
+        elif "nan" in str(self.allele[1]):
+            return
+            #foetus à un pic
+        else:
+            return
+            #foetus à deux pics
+
+
+
 
 class Pere(Patient):
     
-    def __init__(self,marqueur,allele,hauteur):
+    def __init__(self,marqueur,allele,hauteur,informatif):
         super().__init__(marqueur,allele,hauteur)
 
 
@@ -63,29 +91,22 @@ def lecture_donnees(data_frame):
     Allele = Donnees[["Allele 1","Allele 2", "Allele 3"]].values
     Hauteur = Donnees[["Height 1","Height 2", "Height 3"]].values
     for ligne in range(0,Donnees.shape[0] - 1,Iterateur):
-        M = Patient(Donnees["Marker"][ligne],Allele[ligne],Hauteur[ligne])
-        F = Patient(Donnees["Marker"][ligne],Allele[ligne + 1],Hauteur[ligne + 1])
+        M = Mere(Donnees["Marker"][ligne],Allele[ligne],Hauteur[ligne],None,None)
+        F = Foetus(Donnees["Marker"][ligne],Allele[ligne + 1],Hauteur[ligne + 1],None,None,None,None)
         if (Iterateur == 3):
-            P = Patient(Donnees["Marker"][ligne],Allele[ligne + 2],Hauteur[ligne + 2])
+            P = Patient(Donnees["Marker"][ligne],Allele[ligne + 2],Hauteur[ligne + 2],None)
             Donnees_Pere.append(P)
         Donnees_Mere.append(M)
         Donnees_Foetus.append(F)
     return Donnees_Mere, Donnees_Foetus,Donnees_Pere
 
-def foetus_3_pics(foetus):
-    Taille = 16
-    for Alleles in range(Taille):
-        if "nan" not in str(foetus[Alleles].allele):
-            foetus[Alleles] = Foetus(foetus[Alleles].marqueur,foetus[Alleles].allele,foetus[Alleles].hauteur,False,False,"contamine")
-        else:
-            foetus[Alleles] = Foetus(foetus[Alleles].marqueur,foetus[Alleles].allele,foetus[Alleles].hauteur,False,False,"pas_contamine")
-
 
 if __name__ == "__main__":
     M,F,P = lecture_donnees("181985_xfra_ja_200618_PP16.txt")
     verif_concordance(M,F)
-    foetus_3_pics(F)
-    for i in range(16):
-        print(F[i].contamination)
+    print(M[5].allele)
+    print(F[5].allele)
+    A = M[5].allele_semblable(F[5])
+    print(A)
     #machin= Mere("truc","all1",36,True)
     #print(machin.marqueur)
